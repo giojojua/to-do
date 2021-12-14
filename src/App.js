2adf0a9798a007f424a0ca6a61c10209551fbdc7
Gio <jojuagio7@gmail.com>
2021-12-14 04:13:46 +0400

    react app installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+        </div>
+    );
 }
 
 export default App;
