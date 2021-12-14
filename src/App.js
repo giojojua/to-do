0d83cc6ebf87a2fa347299df6ecd1e5987171f67
Gio <jojuagio7@gmail.com>
2021-12-14 06:16:31 +0400

    react app installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <Navbar/>
             </header>
+            <section className="Container">
+
+            </section>
+            <footer className="App-footer">
+                <Footer/>
+            </footer>
         </div>
     );
 }
